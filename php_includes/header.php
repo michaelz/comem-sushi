cdc449d60b7c9cac79b90f610db9b67396b34eb3
Michael Zwyssig <mzwyssig@3ne.ch>
2015-04-24 15:55:49 +0200

    added phpstorm ide files

---
+++ php_includes/header.php
@@ -0,0 +1,50 @@
+
+<div class="logo"><a href="assets/logo.png">logo</a></div>
+<nav class="top-bar" data-topbar="" role="navigation">
+  <ul class="title-area">
+    <!-- Title Area -->
+    <li class="name">
+    </li>
+    <li class="toggle-topbar menu-icon"><a href=""><span>Menu</span></a></li>
+</ul>
+<section class="top-bar-section">
+    <ul class="left">
+      <li class="has-dropdown not-click"><a href="#">Carte</a>
+
+        <ul class="dropdown">
+        	<li class="title back hide-for-large-up"><h5><a href="javascript:void(0)">Retour</a></h5></li>
+          <li><label>Toute notre carte</label></li>
+          <li class="divider"></li>
+          <li><a href="#">Sushis</a></li>
+          <li><a href="#">Boissons</a></li>
+          <li><a href="#">Accompagnements</a></li>
+        </ul>
+      </li>
+
+      <li class="divider"></li>
+			<li><a href="#">Panier</a></li>
+      <li class="divider"></li>
+			<li><a href="#">Mon compte</a></li>
+      <li class="divider"></li>
+			<li class="has-dropdown not-click"><a href="#">Info</a>
+				<ul class="dropdown">
+        	<li class="title back hide-for-large-up"><h5><a href="javascript:void(0)">Retour</a></h5></li>
+          <li><label>Informations</label></li>
+					<li class="title back js-generated">
+						<a href="#">Contact</a>
+					</li>
+					<li>
+						<a href="#">Notre entreprise</a>
+					</li>
+					<li>
+						<a href="#">Horaires</a>
+					</li>
+				</ul>
+			</li>
+    </ul>
+    <!-- Right Nav Section -->
+    <ul class="right">
+      <li class="divider"></li>
+      <li><a href="#">Item 2</a></li>
+    </ul>
+  </section></nav>
