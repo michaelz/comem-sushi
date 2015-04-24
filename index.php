a3d71d20fe0648ba13cef9896f2e8c54f6fc51aa
Michael Zwyssig <mzwyssig@3ne.ch>
2015-04-24 11:02:11 +0200

    added css files and phpized the html structure

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'php_includes/htmlbefore.php'; ?>
+<?php include 'php_includes/header.php'; ?>
+
+
+    <div class="row">
+      <div class="large-12 columns">
+        <h1>Mmh, sushis Ekai.</h1>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="large-12 columns">
+      <p>Bienvenue sur le site !</p>
+        <div class="panel">
+          <p>bloup</p>
+        </div>
+      </div>
+    </div>
+
+<?php include 'php_includes/footer.php'; ?>
+<?php include 'php_includes/htmlafter.php'; ?>
---
+++ php_includes/htmlbefore.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sushi Ekai, Lausanne</title>
+    <link rel="stylesheet" href="css/app.css" />
+    <script src="bower_components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
