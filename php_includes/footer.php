df7ba4a384a8dc80598c1fdba47221582b816163
Michael Zwyssig <mzwyssig@3ne.ch>
2015-04-25 18:23:44 +0200

    header structure

--- php_includes/footer.php
+++ php_includes/footer.php
@@ -1,3 +1,3 @@
-<footer>
-
+<footer class="row">
+	contenu du footer
 </footer>
--- php_includes/header.php
+++ php_includes/header.php
@@ -1,50 +1,68 @@
+<div class="row top-container">
+  <div class="columns small-4 medium-3 large-2 logo"><a href="assets/logo.png">logo</a></div>
+  <header class="header columns small-8 medium-9 large-10">
+    <div class="row">
+      <div class="phone columns medium-5">
+        022 345 67 89
+      </div>
+      <div class="search show-for-medium-up columns medium-7 ">
+        <div class="columns medium-8">
+              <label for="right-label" class="right inline">Label</label>
+        </div>
+        <div class="columns medium-4">
+                <input type="text" id="right-label" placeholder="Inline Text Input">
+        </div>
+      </div>
+    </div>
 
-<div class="logo"><a href="assets/logo.png">logo</a></div>
-<nav class="top-bar" data-topbar="" role="navigation">
-  <ul class="title-area">
-    <!-- Title Area -->
-    <li class="name">
-    </li>
-    <li class="toggle-topbar menu-icon"><a href=""><span>Menu</span></a></li>
-</ul>
-<section class="top-bar-section">
-    <ul class="left">
-      <li class="has-dropdown not-click"><a href="#">Carte</a>
+    <nav class="row top-bar" data-topbar="" role="navigation">
+      <ul class="title-area">
+        <!-- Title Area -->
+        <li class="name">
+        </li>
+        <li class="toggle-topbar menu-icon"><a href=""><span>Menu</span></a></li>
+      </ul>
+      <section class="top-bar-section">
+        <ul class="left">
+          <li class="has-dropdown not-click"><a href="#">Carte</a>
 
-        <ul class="dropdown">
-        	<li class="title back hide-for-large-up"><h5><a href="javascript:void(0)">Retour</a></h5></li>
-          <li><label>Toute notre carte</label></li>
+            <ul class="dropdown">
+              <li class="title back hide-for-large-up"><h5><a href="javascript:void(0)">Retour</a></h5></li>
+              <li><label>Toute notre carte</label></li>
+              <li class="divider"></li>
+              <li><a href="#">Sushis</a></li>
+              <li><a href="#">Boissons</a></li>
+              <li><a href="#">Accompagnements</a></li>
+            </ul>
+          </li>
+
           <li class="divider"></li>
-          <li><a href="#">Sushis</a></li>
-          <li><a href="#">Boissons</a></li>
-          <li><a href="#">Accompagnements</a></li>
+          <li><a href="#">Panier</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Mon compte</a></li>
+          <li class="divider"></li>
+          <li class="has-dropdown not-click"><a href="#">Info</a>
+            <ul class="dropdown">
+              <li class="title back hide-for-large-up"><h5><a href="javascript:void(0)">Retour</a></h5></li>
+              <li><label>Informations</label></li>
+              <li class="title back js-generated">
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Notre entreprise</a>
+              </li>
+              <li>
+                <a href="#">Horaires</a>
+              </li>
+            </ul>
+          </li>
         </ul>
-      </li>
-
-      <li class="divider"></li>
-			<li><a href="#">Panier</a></li>
-      <li class="divider"></li>
-			<li><a href="#">Mon compte</a></li>
-      <li class="divider"></li>
-			<li class="has-dropdown not-click"><a href="#">Info</a>
-				<ul class="dropdown">
-        	<li class="title back hide-for-large-up"><h5><a href="javascript:void(0)">Retour</a></h5></li>
-          <li><label>Informations</label></li>
-					<li class="title back js-generated">
-						<a href="#">Contact</a>
-					</li>
-					<li>
-						<a href="#">Notre entreprise</a>
-					</li>
-					<li>
-						<a href="#">Horaires</a>
-					</li>
-				</ul>
-			</li>
-    </ul>
-    <!-- Right Nav Section -->
-    <ul class="right">
-      <li class="divider"></li>
-      <li><a href="#">Item 2</a></li>
-    </ul>
-  </section></nav>
+        <!-- Right Nav Section -->
+        <ul class="right">
+          <li class="divider"></li>
+          <li><a href="#">Item 2</a></li>
+        </ul>
+      </section>
+    </nav>
+  </header>
+</div>
